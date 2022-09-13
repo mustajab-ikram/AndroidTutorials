7061674d0decd824b92ed911badb50edd428ebd3
Mustajab Ikram <mustajabdelhi@gmail.com>
2022-09-13 22:32:58 +0530

    Android Studio project folders description improved

--- app/src/main/java/com/example/androidtutorials/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorials/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.androidtutorials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Methods", "onCreate() is called")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("Methods", "onStart() is called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("Methods", "onResume() is called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("Methods", "onPause() is called")
+
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("Methods", "onStop() is called")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Methods", "onRestart() is called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Methods", "onDestroy() is called")
     }
 }

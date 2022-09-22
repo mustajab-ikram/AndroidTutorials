242fd2abc58f98267d12eeda2d9eb60cb4e6ad28
Mustajab Ikram <mustajabdelhi@gmail.com>
2022-09-22 23:53:11 +0530

    LinearLayout Exercise

--- app/src/main/java/com/example/androidtutorials/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorials/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.androidtutorials
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
